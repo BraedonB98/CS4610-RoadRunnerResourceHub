bcb331140b13e95f7a1f2868be7536da0ddaa1dc
Braedon Bellamy <braedonb98@gmail.com>
2024-09-19 14:36:53 -0600

    adding card to freshman page

--- roadrunner-resource-hub-frontend/src/student_resources/pages/Freshman.js
+++ roadrunner-resource-hub-frontend/src/student_resources/pages/Freshman.js
@@ -1,11 +1,14 @@
 import React  from "react";
+import Card from "../../shared/components/UIElements/Card";
 
 const Freshman = () => {
     //insert function here
     return (
         <React.Fragment>
+            <Card>
             <h1>Welcome Freshman</h1>
             <p>Here are some resources you may find useful </p>
+            </Card>
         </React.Fragment>
     )
 }
