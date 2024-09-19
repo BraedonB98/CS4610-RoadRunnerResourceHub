e69618b2842e327cf0674dc3fe130e4d435fcfe6
Joseph <jtewold1@msudenver.edu>
2024-09-19 14:22:04 -0600

    Changed font color for side bar for mobile view

--- roadrunner-resource-hub-frontend/src/landing/pages/Welcome.js
+++ roadrunner-resource-hub-frontend/src/landing/pages/Welcome.js
@@ -1,11 +1,12 @@
 import React  from "react";
 
 const Welcome = () => {
-    //insert function here
     return (
         <React.Fragment>
             <h1>Welcome to the Roadrunner Resource Hub!</h1>
-            <p>Here you will find resources for all your academic needs. Whether you are a freshman sophomore, junior or a senior </p>
+            <p>Here you will find common resources for all your academic needs. 
+                Whether you are a freshman sophomore, junior or a senior </p>
+
         </React.Fragment>
     )
 }
