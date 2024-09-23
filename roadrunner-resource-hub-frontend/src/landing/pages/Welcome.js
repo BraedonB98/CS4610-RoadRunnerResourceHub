d02048f34cd0568c32a8ab85a1aa406cf803b775
Joseph <jtewold1@msudenver.edu>
2024-09-22 22:47:45 -0600

    added padding to welcome text for each page with student resources css file

--- roadrunner-resource-hub-frontend/src/landing/pages/Welcome.js
+++ roadrunner-resource-hub-frontend/src/landing/pages/Welcome.js
@@ -1,13 +1,18 @@
 import React  from "react";
 import Card from "../../shared/components/UIElements/Card";
-import '../../student_resources/pages/styling/pages.css';
+import '../../student_resources/pages/styling/StudentResources.css';
 
 const Welcome = () => {
     return (
         <React.Fragment>
-            <h1>Welcome to the Roadrunner Resource Hub!</h1>
-            <p>Here you will find common resources for all your academic needs. 
-                Whether you are a freshman sophomore, junior or a senior </p>
+
+            <div className="page-welcometext">
+
+                <h1>Welcome to the Roadrunner Resource Hub!</h1>
+                <p>Here you will find common resources for all your academic needs. 
+                    Whether you are a freshman sophomore, junior or a senior </p>
+
+            </div>
 
             <div className="welcome-container">
 
--- roadrunner-resource-hub-frontend/src/student_resources/pages/Sophomore.js
+++ roadrunner-resource-hub-frontend/src/student_resources/pages/Sophomore.js
@@ -1,11 +1,14 @@
 import React  from "react";
+import '../../student_resources/pages/styling/StudentResources.css';
 
 const Sophomore = () => {
     //insert function here
     return (
         <React.Fragment>
-            <h1>Welcome Sophomores</h1>
-            <p>Here are some resources you may find useful </p>
+            <div className = "page-welcometext">
+                <h1>Welcome Sophomores</h1>
+                <p>Here are some resources you may find useful </p>
+            </div>
         </React.Fragment>
     )
 }
