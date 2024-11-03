b57e66fee729e15d22f2a223cef341beab242e99
BraedonB98 <braedonb98@gmail.com>
2024-11-03 14:14:03 -0700

    Nav Bar Changes -adding login -adding env files for images -noting for user context for authentication later

--- roadrunner-resource-hub-frontend/src/shared/context/user-context.js
+++ roadrunner-resource-hub-frontend/src/shared/context/user-context.js
@@ -4,7 +4,7 @@ export const UserContext = createContext({
   name: null,
   email: null,
   userId: null,
-  imageUrl: `data/uploads/images/default.svg`,
+  imageUrl: `data/uploads/images/default.svg`,//will need to add Authorization level to user context -> then make add new resource only appear on appropriate pages
   setUser: () => {},
   removeUser: () => {},
 });
